bcef3883e01fa1f3b86462af0fbc1f4f50c7573f
E/S Pronk <eddy@autofilltech.com>
2022-01-13 01:43:17 +0100

    FFT, not working 100%

--- main.cu
+++ main.cu
@@ -5,23 +5,61 @@
 #include "audiodevice.h"
 #include "mididevice.h"
 
-__global__ static void f_makeTone(float* output, size_t channels, size_t samples, size_t sr, size_t t, float v)
+__global__ static void f_makeTone(cufftComplex* output, size_t samples, size_t sr, size_t t, float v)
 {
 	auto stride = gridDim * blockDim;
 	auto offset = blockDim * blockIdx + threadIdx;
 
-	assert(2 == channels);
 	assert(1 == stride.y);
 	assert(0 == offset.y);
 
-	auto d = (float2*)output;
 	for (auto s = offset.x; s < samples; s += stride.x)
 	{
 		float a1 = (t + s) % sr;
-		float a2 = (t + s + (sr >> 1)) % sr;
 		float b1 = v * fmaf(powf((sr-a1)/sr,10), fmodf(a1/40,2), -1);
-		float b2 = v * fmaf(powf((sr-a2)/sr,10), fmodf(a2/40,2), -1);
-		d[s] = {b1,b2};
+		output[s] = {b1,0};
+	}
+}
+
+__global__ static void f_makeImpulseResponse(cufftComplex* output, size_t samples, size_t sr, size_t t, float v)
+{
+	auto stride = gridDim * blockDim;
+	auto offset = blockDim * blockIdx + threadIdx;
+
+	assert(1 == stride.y);
+	assert(0 == offset.y);
+
+	for (auto s = offset.x; s < samples; s += stride.x)
+	{
+		output[s] = {s == 1 ? 1.0f : 0};
+	}
+}
+
+__global__ static void f_pointwiseAdd(cufftComplex* r, const cufftComplex* a, const cufftComplex* b, size_t size)
+{
+	auto stride = gridDim * blockDim;
+	auto offset = blockDim * blockIdx + threadIdx;
+
+	for (auto s = offset.x; s < size; s += stride.x)
+	{
+		auto va = a[s];
+		auto vb = b[s];
+		r[s] = va + vb;
+	}
+}
+
+__global__ static void f_pointwiseMultiply(cufftComplex* r, const cufftComplex* a, const cufftComplex* b, size_t size)
+{
+	auto stride = gridDim * blockDim;
+	auto offset = blockDim * blockIdx + threadIdx;
+
+	for (auto s = offset.x; s < size; s += stride.x)
+	{
+		auto va = a[s];
+		auto vb = b[s];
+		auto re = va.x * vb.x - va.y * vb.y;
+		auto im = (va.x + va.y) * (vb.x + vb.y) - re;
+		r[s] = make_float2(re, im) / size;
 	}
 }
 
@@ -34,6 +72,12 @@ public:
 		_b(nullptr), _bfft(nullptr),
 		_r(nullptr), _rfft(nullptr)
 	{
+		int rc;
+		for (auto i = 0; i<4; i++) 
+		{
+			rc = cudaStreamCreateWithFlags(&_streams[i], cudaStreamNonBlocking);
+			assert(0 == rc);
+		}
 	}
 
 	void prepare(size_t size, size_t channels)
@@ -43,23 +87,37 @@ public:
 		assert(size > 0);
 		assert(channels > 0);
 
-		_fftSize = size * channels * sizeof(cufftComplex);
-		rc = cudaMalloc(&_a, _fftSize);
+		auto fftSize = size * sizeof(cufftComplex);
+		rc = cudaMalloc(&_a, fftSize);
 		assert(0 == rc);
-		rc = cudaMalloc(&_b, _fftSize);
+		rc = cudaMalloc(&_b, fftSize);
 		assert(0 == rc);
-		rc = cudaMalloc(&_r, _fftSize);
+		rc = cudaMalloc(&_r, fftSize);
 		assert(0 == rc);
 
-		rc = cudaMalloc(&_afft, _fftSize);
+		rc = cudaMalloc(&_afft, fftSize);
 		assert(0 == rc);
-		rc = cudaMalloc(&_bfft, _fftSize);
+		rc = cudaMalloc(&_bfft, fftSize);
 		assert(0 == rc);
-		rc = cudaMalloc(&_rfft, _fftSize);
+		rc = cudaMalloc(&_rfft, fftSize);
 		assert(0 == rc);
-	}
-
+		rc = cudaMalloc(&_residual, fftSize);
+		assert(0 == rc);
 	
+		_fftSize = size;
+		int n[] = { (int)_fftSize };
+		int iembed[] = { (int)_fftSize };
+		int oembed[] = { (int)_fftSize };
+		int istride = 1;
+		int ostride = 1;
+		int batch = 1;
+
+		rc = cufftPlanMany(&_plan, 1, n, 
+				iembed, istride, _fftSize, 
+				oembed, ostride, _fftSize,
+				CUFFT_C2C, batch);
+		assert(0 == rc);
+	}	
 
 protected:
 	virtual void midiDeviceHandlerOnReceive(MidiDevice* sender, const uint8_t* buffer, size_t len) override
@@ -69,19 +127,47 @@ protected:
 
 	virtual void audioDeviceHandlerOnOutputBuffer(AudioDevice* sender, float* buffer, size_t frames) override
 	{
+		int rc;
 		static size_t t = 0;
 		auto nc = sender->numChannels;
 		auto sr = sender->sampleRate;
-		f_makeTone <<< 2, 256, 0, 0 >>> (buffer, nc, frames, sr, t, 0.15f);
-		cudaStreamSynchronize(0);
+		f_makeTone <<< 2, 256, 0, _streams[0] >>> (_a, frames, sr, t, 0.15f);
+		f_makeImpulseResponse <<< 4, 256, 0, _streams[1] >>> (_b, 4096, sr, 0, 1.0f);
+		cudaStreamSynchronize(_streams[0]);
+		cudaStreamSynchronize(_streams[1]);
+		cufftSetStream(_plan, _streams[0]);
+
+		rc = cufftExecC2C(_plan, _a, _afft, CUFFT_FORWARD);
+		assert(cudaSuccess == rc);
+		rc = cufftExecC2C(_plan, _b, _bfft, CUFFT_FORWARD);
+		assert(cudaSuccess == rc);
+
+		rc = cudaMemcpyAsync(_residual, _r+frames, frames*sizeof(cufftComplex), cudaMemcpyDeviceToDevice, _streams[0]);
+		assert(cudaSuccess == rc);
+		
+		f_pointwiseMultiply <<< 8, 256, 0, _streams[0] >>> (_rfft, _afft, _bfft, _fftSize);
+		
+		rc = cufftExecC2C(_plan, _rfft, _r, CUFFT_INVERSE);
+		assert(cudaSuccess == rc);
+
+		f_pointwiseAdd <<< 4, 256, 0, _streams[0] >>> (_a, _r, _residual, frames);
+
+		rc = cudaMemcpyAsync(buffer, _r, frames*sizeof(cufftComplex), cudaMemcpyDeviceToHost, _streams[0]);
+		assert(cudaSuccess == rc);
+
+		rc = cudaStreamSynchronize(_streams[0]);
+		assert(cudaSuccess == rc);
 		t += frames;
 	}
 
 private:
+	cufftHandle _plan;
 	cufftComplex *_a, *_afft;
 	cufftComplex *_b, *_bfft;
 	cufftComplex *_r, *_rfft;
+	cufftComplex *_residual;
 
+	cudaStream_t _streams[4];
 	size_t _fftSize;
 
 };
@@ -91,6 +177,7 @@ int main()
 	selectGpu();
 
 	MainHandler handler;
+	handler.prepare(4096, 2);
 
 	AudioDevice sound("default", &handler);
 	sound.start();
